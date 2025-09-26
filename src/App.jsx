65bf89106843f3e2d0f4017e82f2726b6b8bb9dd
devopslayer <layerdevops@gmail.com>
2025-09-26 12:04:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>My Portfolio</h1>
+      <p>
+        Welcome to my portfolio website. Here you can find my latest projects
+        and skills.
+      </p>
+    </>
+  );
+}
+
+export default App;
